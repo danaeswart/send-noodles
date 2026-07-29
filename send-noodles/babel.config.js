bc2b6d24f8f693bfe3c4b7840dc914255c168f16
danaeswart <241022@virtualwindow.co.za>
2026-07-29 17:43:49 +0200

    initial app creation witll files and packages downloaded

---
+++ send-noodles/babel.config.js
@@ -0,0 +1,9 @@
+﻿module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    // react-native-reanimated/plugin MUST be listed last — this is a hard
+    // requirement from the Reanimated docs, not a style choice.
+    plugins: ['react-native-reanimated/plugin'],
+  };
+};
